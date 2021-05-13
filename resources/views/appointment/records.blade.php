4d44a8426c7a9eccdd1401d90cfd4d4a0df7198a
farhan-helmy <farhanhlmy@gmail.com>
2021-05-13 21:43:59 +0800

    sadasdas

--- resources/views/appointment/records.blade.php
+++ resources/views/appointment/records.blade.php
@@ -8,11 +8,11 @@
     <div class="page-breadcrumb">
         <div class="row">
             <div class="col-7 align-self-center">
-                <h3 class="page-title text-truncate text-dark font-weight-medium mb-1">Patient Records</h3>
+                <h3 class="page-title text-truncate text-dark font-weight-medium mb-1">Appointment Records</h3>
                 <div class="d-flex align-items-center">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb m-0 p-0">
-                            <li class="breadcrumb-item"><a href="index.html">Patient Records</a>
+                            <li class="breadcrumb-item"><a href="index.html">Appointment Records</a>
                             </li>
                         </ol>
                     </nav>
